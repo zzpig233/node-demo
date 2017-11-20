ea293214040cdf47a272e6db694db03bd3b24817
zzpig <203360009@qq.com>
2017-11-20 14:08:47 +0800

    server.js作业

--- server.js
+++ server.js
@@ -20,22 +20,22 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if(path == '/style.js'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
-    response.end()
-  }else if(path == '/script.html'){
+  if(path == '/main.js'){
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else if(path == '/index.css'){
+  }else if(path == '/style.css'){
+    response.setHeader('Content-Type', 'text/css; charset=utf-8')
+    response.write('body{background-color: #ddd;}h1{color: red;}')
+    response.end()
+  }else if(path == '/'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.js">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/script.html"></script>' +
-      '</body></html>')
+    response.write('<!DOCTYPE>\n<html>' + 
+        '<head><link rel="stylesheet" href="/style.css">' + 
+        '</head><body>' + 
+        '<h1>Hello</h1>' +
+        '<script src="/main.js"></script>' +
+        '</body></html>')  
     response.end()
   }else{
     response.statusCode = 404
